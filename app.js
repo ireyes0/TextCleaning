d6a61ae85f331723e1ae285c6d4c1fb0eb3026db
ireyes0 <ireyes.network@gmail.com>
2025-07-31 17:26:40 -0600

    INTEGRANTES DEL EQUIPO

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//INTEGRANTES DEL EQUIPO:
+//INTEGRANTES DEL EQUIPO
 // - Sebastian Badillo Romo
 // - Jose de Jesus Bernal Munoz
 // - Lazaro Roberto Luevano Serna
 // - Carlos Isaac Reyes Martinez
+
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
